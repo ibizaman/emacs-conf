05882a350962255250f245586f382a03758848c0
Pierre Penninckx <ibizapeanut@gmail.com>
2020-12-09 15:50:38 -0800

    comment out inexistent magit sections

--- init.el
+++ init.el
@@ -143,8 +143,8 @@
   :config
   (setq magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1
 		magit-published-branches nil)
-  (magit-add-section-hook 'magit-status-sections-hook 'forge-insert-assigned-pullreqs nil t)
-  (magit-add-section-hook 'magit-status-sections-hook 'forge-insert-assigned-issues nil t)
+  ;; (magit-add-section-hook 'magit-status-sections-hook 'forge-insert-assigned-pullreqs nil t)
+  ;; (magit-add-section-hook 'magit-status-sections-hook 'forge-insert-assigned-issues nil t)
   (evil-define-key 'normal magit-mode-map (kbd "o") 'magit-visit-thing)
   (evil-define-key 'normal magit-mode-map (kbd "O") 'magit-diff-visit-file-other-window))
 
