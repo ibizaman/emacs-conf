048ae1b0844c272721fa06ef164b61ed0317b47a
ibzaman <ibizapeanut@gmail.com>
2020-12-17 09:21:53 -0800

    add custom set faces

--- init.el
+++ init.el
@@ -1050,3 +1050,16 @@ _p_:   ... in _p_ackage   _N_:        ^^... in package
 						 :use-authinfo t)))
   (elfeed-set-timeout 36000)
   (elfeed-protocol-enable))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:foreground "#df9f2d" :underline t :weight bold :height 1.3))))
+ '(org-level-2 ((t (:foreground "#df9f2d" :underline t :weight normal :height 1.3))))
+ '(org-level-3 ((t (:foreground "#b1d631" :underline t :weight bold :height 1.3))))
+ '(org-level-4 ((t (:foreground "#b1d631" :underline t :weight normal :height 1.3))))
+ '(org-level-5 ((t (:foreground "#b1d631" :height 1.1))))
+ '(org-level-6 ((t (:foreground "#5091a1" :underline t :weight bold :height 1.1))))
+ '(org-level-7 ((t (:foreground "#5091a1" :underline t :height 1.1))))
+ '(org-level-8 ((t (:foreground "#5091a1" :height 1.1)))))
