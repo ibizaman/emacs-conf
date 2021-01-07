96e73df71d287d784d554ad259f5167220e18568
Pierre Penninckx <pierre@fastly.com>
2021-01-07 09:32:07 -0800

    hide modeline

--- init.el
+++ init.el
@@ -288,6 +288,52 @@
 
 (column-number-mode 1)
 
+;; Hide modeline
+;; See http://bzg.fr/emacs-hide-mode-line.html
+(progn
+  (defvar-local hidden-mode-line-mode nil)
+  (defvar-local hide-mode-line nil)
+
+  (define-minor-mode hidden-mode-line-mode
+    "Minor mode to hide the mode-line in the current buffer."
+    :init-value nil
+    :global nil
+    :variable hidden-mode-line-mode
+    :group 'editing-basics
+    (if hidden-mode-line-mode
+        (setq hide-mode-line mode-line-format
+              mode-line-format nil)
+      (setq mode-line-format hide-mode-line
+            hide-mode-line nil))
+    (force-mode-line-update)
+    ;; Apparently force-mode-line-update is not always enough to
+    ;; redisplay the mode-line
+    (redraw-display)
+    (when (and (called-interactively-p 'interactive)
+               hidden-mode-line-mode)
+      (run-with-idle-timer
+       0 nil 'message
+       (concat "Hidden Mode Line Mode enabled.  "
+               "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+  ;; Activate hidden-mode-line-mode
+  (hidden-mode-line-mode 1)
+  (add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+
+  ;; Command to toggle the display of the mode-line as a header
+  (defvar-local header-line-format nil)
+  (defun mode-line-in-header ()
+    (interactive)
+	(if (not header-line-format)
+		(setq header-line-format (if hidden-mode-line-mode hide-mode-line mode-line-format)
+			  mode-line-format nil)
+	  (if hidden-mode-line-mode
+		  (setq hide-mode-line header-line-format)
+		(setq mode-line-format header-line-format))
+	  (setq header-line-format nil)
+	(set-window-buffer nil (current-buffer)))
+  (global-set-key (kbd "C-s-SPC") 'mode-line-in-header)))
+
 ;;;; Theme
 
 (use-package mustang-theme
