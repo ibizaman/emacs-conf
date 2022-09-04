81d87b8fc1a92a6d563b707ca32ab3c171340ede
ibizaman <ibizapeanut@gmail.com>
2022-09-04 09:06:24 -0700

    add org-visibility

--- init.el
+++ init.el
@@ -205,6 +205,33 @@
 (use-package pdf-tools
   :straight t)
 
+(use-package org-visibility
+  :after (org)
+  :straight t
+  ;; :bind* (:map org-visibility-mode-map
+  ;;              ("C-x C-v" . org-visibility-force-save) ; defaults to `find-alternative-file'
+  ;;              ("C-x M-v" . org-visibility-remove))    ; defaults to undefined
+  :hook (org-mode . org-visibility-mode)
+  :custom
+  ;; optionally change the location of the state file
+  ;;(org-visibility-state-file `,(expand-file-name "/some/path/.org-visibility"))
+  ;; list of directories and files to persist and restore visibility state of
+  ;; (org-visibility-include-paths `(,(file-truename "~/.emacs.d/init-emacs.org")
+  ;;                                 ,(file-truename "~/org")))
+  ;; persist all org files regardless of location
+  (org-visibility-include-regexps '("\\.org\\'"))
+  ;; list of directories and files to not persist and restore visibility state of
+  ;;(org-visibility-exclude-paths `(,(file-truename "~/org/old")))
+  ;; optionally set maximum number of files to keep track of
+  ;; oldest files will be removed from the state file first
+  (org-visibility-maximum-tracked-files 100)
+  ;; optionally set maximum number of days (since saved) to keep track of
+  ;; files older than this number of days will be removed from the state file
+  (org-visibility-maximum-tracked-days 180)
+  ;; optionally turn off visibility state change messages
+  ;;(org-visibility-display-messages nil)
+  )
+
 
 ;;; Base packages
 
