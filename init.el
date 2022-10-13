892bc83b9c64814f9215d82fe97701fb1a302b75
Pierre Penninckx <pierre@fastly.com>
2022-10-13 09:37:47 -0700

    add more flycheck tweaks

--- init.el
+++ init.el
@@ -388,8 +388,35 @@
   :delight company-mode
   :config
   (setq company-idle-delay 0
-        company-minimum-prefix-length 1)
+        company-minimum-prefix-length 1
+        company-tooltip-align-annotations t
+        company-insertion-triggers nil
+        company-format-margin-function 'company-vscode-dark-icons-margin
+        company-frontends '(company-pseudo-tooltip-unless-just-one-frontend
+                            company-echo-metadata-frontend
+                            company-preview-if-just-one-frontend)
+        company-search-regexp-function 'company-search-words-in-any-order-regexp)
   (add-hook 'after-init-hook 'global-company-mode))
+
+;;; https://github.com/clojure-emacs/cider/issues/2908#issuecomment-709691997
+(with-eval-after-load 'company
+  (add-hook 'evil-local-mode-hook
+            (lambda ()
+              ;; Note:
+              ;; Check if `company-emulation-alist' is in
+              ;; `emulation-mode-map-alists', if true, call
+              ;; `company-ensure-emulation-alist' to ensure
+              ;; `company-emulation-alist' is the first item of
+              ;; `emulation-mode-map-alists', thus has a higher
+              ;; priority than keymaps of evil-mode.
+              ;; We raise the priority of company-mode keymaps
+              ;; unconditionally even when completion is not
+              ;; activated. This should not cause problems,
+              ;; because when completion is activated, the value of
+              ;; `company-emulation-alist' is ((t . company-my-keymap)),
+              ;; when completion is not activated, the value is ((t . nil)).
+              (when (memq 'company-emulation-alist emulation-mode-map-alists)
+                (company-ensure-emulation-alist)))))
 
 (use-package company-posframe
   :straight t
