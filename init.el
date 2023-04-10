07f4556c95be137c5bf5b5a26d085ee4ddc181e6
Pierre Penninckx <pierre@fastly.com>
2023-04-10 08:34:18 -0700

    add ejira package

--- init.el
+++ init.el
@@ -1727,6 +1727,48 @@ _p_:   ... in _p_ackage   _N_:        ^^... in package   _d_: debug RUN
 ; (setq request-log-level 'blather)
 ; (setq request-message-level 'blather)
 
+(use-package ejira
+  :if (seq-some (lambda (name) (string= system-name name)) '("Pierres-MBP.tiserbox.com" "C02Z93RNLVDL"))
+  :straight (ejira :type git :host github :repo "nyyManni/ejira" :branch "master")
+  :init
+  (setq jiralib2-url              "https://fastly.atlassian.net"
+        jiralib2-auth             'basic
+        jiralib2-user-login-name  "pierre@fastly.com"
+        jiralib2-token            nil
+
+        ;; NOTE, this directory needs to be in `org-agenda-files'`
+        ejira-org-directory       "~/Documents/ejira"
+        ejira-projects            '("SDS")
+
+        ejira-priorities-alist    '(("Highest" . ?A)
+                                    ("High"    . ?B)
+                                    ("Medium"  . ?C)
+                                    ("Low"     . ?D)
+                                    ("Lowest"  . ?E))
+        ejira-todo-states-alist   '(("To Do"       . 1)
+                                    ("In Progress" . 2)
+                                    ("Done"        . 3)))
+  :config
+  ;; Tries to auto-set custom fields by looking into /editmeta
+  ;; of an issue and an epic.
+  (add-hook 'jiralib2-post-login-hook #'ejira-guess-epic-sprint-fields)
+
+  ;; They can also be set manually if autoconfigure is not used.
+  ;; (setq ejira-sprint-field       'customfield_10001
+  ;;       ejira-epic-field         'customfield_10002
+  ;;       ejira-epic-summary-field 'customfield_10004)
+
+  (require 'ejira-agenda)
+
+  ;; Make the issues visisble in your agenda by adding `ejira-org-directory'
+  ;; into your `org-agenda-files'.
+  (add-to-list 'org-agenda-files ejira-org-directory)
+
+  ;; Add an agenda view to browse the issues that
+  (org-add-agenda-custom-command
+   '("j" "My JIRA issues"
+     ((ejira-jql "resolution = unresolved and assignee = currentUser()"
+                 ((org-agenda-overriding-header "Assigned to me")))))))
 ;;; Save session
 
 ; (desktop-load-default)
